a6c5b84884c8a49923a489d15b9d30d6da7efea8
waspryo <wasp2123321@gmail.com>
2019-12-01 14:51:33 +0900

    create POST

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const app = document.querySelector('div#app');
 
  // GET 
 axios.get('https://jsonplaceholder.typicode.com/users')
 .then(res => {
     for(let user in res.data){
-        app.innerHTML = res.data[user].name;
+        app.innerHTML = `<h1>Name: ${res.data[user].name}</h1>`;
         console.log(res.data)
     }
 });
+
+// POST
+axios.post("https://jsonplaceholder.typicode.com/users", {
+    name: "Alvee",
+    email: "veeweb@gmail.com",
+    phone: "12391283"
+},{
+    responseMethod: "POST"
+})
+
+.then(res => {
+    console.log(res)
+})
